0cce94fd8b6af5635420050da4bc2eec11272f90
dwijgohil24 <dwijgohil24@gmail.com>
2021-08-22 18:41:59 +0530

    my first flutter app commit on github

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Center(child: Text('Dicee App')),
+          backgroundColor: Colors.purple,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber=1;
+  int rightDiceNumber=1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children:
+        [
+          Expanded(
+            child:TextButton(
+                onPressed: ( ){
+                  setState(() {
+                     leftDiceNumber= Random().nextInt(6)+1;
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')),
+          ),
+
+          Expanded(
+            child:TextButton(
+                onPressed: ( ){
+                  setState(() {
+                    rightDiceNumber= Random().nextInt(6)+1;
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
